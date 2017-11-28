64395653515886681ad89e41edc1f94e152ff6b8
ADMIN <ADMIN@ASUS>
2017-11-28 13:34:04 +0530

    first

---
+++ src/main/java/demo_upload/upload.java
@@ -0,0 +1,21 @@
+package demo_upload;
+
+
+
+import java.io.*;  
+import javax.servlet.ServletException;  
+import javax.servlet.http.*;  
+import com.oreilly.servlet.MultipartRequest;  
+  
+public class upload extends HttpServlet {  
+  
+public void doPost(HttpServletRequest request, HttpServletResponse response)  
+    throws ServletException, IOException {  
+  
+response.setContentType("text/html");  
+PrintWriter out = response.getWriter();  
+          
+MultipartRequest m=new MultipartRequest(request,"E:/upload");  
+out.print("successfully uploaded");  
+}  
+}  
